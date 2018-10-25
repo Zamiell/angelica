9d75bf4272a9d3f55d2effd0e1979607b720f3c6
James Nesta <j.nesta@gmail.com>
2018-10-25 23:02:39 +0000

    2018

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # angelica
 
 She's a real girl.
+
+<br />
+
+Setup
+-----
+
+`apt install python python-pip`
+`pip install cobe`
--- angelica.js
+++ angelica.js
@@ -1,20 +1,38 @@
+'use strict';
+
 // Imports
-var tmi  = require('tmi.js');                  // The bot uses the tmi-js library to connect to Twitch
-var fs   = require('fs');                      // For getting passwords
-var exec = require('child_process').spawnSync; // For interacting with the brain
+const tmi  = require('tmi.js');                  // The bot uses the tmi-js library to connect to Twitch
+const fs   = require('fs');                      // For getting passwords
+const exec = require('child_process').spawnSync; // For interacting with the brain
 
 // General configuration
 var botDirectory = '/root/angelica';
 var botName = 'Angelica_E';
+var channelUserList = [
+    '#zamiell',
+    '#thalen22',
+    '#stoneagemarcus',
+    '#lexicalpedant',
+    '#sednegi',
+    '#skylawinters',
+];
 var ignoreList = [
     'nightbot',
     'zamielbot',
     'sneewo',
     'zeldobot',
+    'lexicalbot',
+    'mikuia',
+    'stoneage_bot',
+    'moobot',
+    'isaacracingplus',
+];
+var incubationList = [
+    'sednegi',
 ];
 
 // Twitch configuration
-var oauth = fs.readFileSync(botDirectory + '/passwords/Twitch.txt', 'utf8').trim();
+let oauth = fs.readFileSync(botDirectory + '/passwords/Twitch.txt', 'utf8').trim();
 var TwitchBot = new tmi.client({
     options: {
         debug: true,
@@ -26,18 +44,17 @@ var TwitchBot = new tmi.client({
         username: botName,
         password: oauth,
     },
-    channels: ['#zamiell'],
+    channels: channelUserList,
 });
-var channelUserList = {};
 
 // Start the server
-var datetime = new Date();
-console.log('----- STARTING ANGELTICA @ ' + datetime + ' for 1 user! -----');
+let datetime = new Date();
+console.log('----- STARTING ANGELTICA @ ' + datetime + ' for ' + 1 + 'users! -----');
 TwitchBot.connect();
 
 // General purpose functions
 function error(message) {
-    var datetime = new Date();
+    let datetime = new Date();
     message = datetime + ' - ' + message;
     console.error(message);
     console.log(message);
@@ -45,6 +62,9 @@ function error(message) {
 
 // Catch chat messages
 TwitchBot.on('chat', function(channel, user, message, self) {
+    // Local variables
+    let cmd;
+
     // Since user is an object containing various things, just make it equal to the username for simplicity
     user = user.username; // See: https://www.tmijs.org/docs/Events.md#chat
 
@@ -53,8 +73,8 @@ TwitchBot.on('chat', function(channel, user, message, self) {
         return;
     }
 
-    // Ignore bot messages
-    for (var i = 0; i < ignoreList.length; i++) {
+    // Ignore messages from other bots
+    for (let i = 0; i < ignoreList.length; i++) {
         if (user.toLowerCase() === ignoreList[i]) {
             return;
         }
@@ -62,31 +82,36 @@ TwitchBot.on('chat', function(channel, user, message, self) {
 
     // Ignore people typing commands
     if (message.substring(0, 1) === '!') {
-        return
+        return;
     }
 
     // Remove whitespace from both sides of the string
     message = message.trim();
 
     // Log all messages
-    var datetime = new Date();
+    let datetime = new Date();
     console.log(datetime + ' - TWITCH [' + channel + '] <' + user + '> ' + message);
 
     // Add the message to the brain
-    var brain = channel.match(/#(.+)/)[1];
-    var cmd = botDirectory + '/scripts/teach-brain.py';
+    let brain = channel.match(/#(.+)/)[1];
+    cmd = botDirectory + '/scripts/teach-brain.py';
     exec(cmd, [brain, message]);
 
     // Add the message to the brain backup
     //fs.appendFileSync(botDirectory + '/brains/' + brain + '-log.txt', message + '\n');
 
+    // Return if we are in incubation mode
+    if (incubationList.indexOf(channel) !== -1) {
+        return;
+    }
+
     // Randomly decide if the bot should respond
-    botResponding = false;
-    if (Math.random() < 0.05) { // 5% chance
+    let botResponding = false;
+    if (Math.random() < 0.025) { // 2.5% chance
         botResponding = true;
-    } else if (message.toLowerCase().indexOf('angel') > -1 && Math.random() < 0.33) { // 33% chance if she's being talked to
+    } else if (message.toLowerCase().indexOf('angelica') > -1 && Math.random() < 0.25) { // 25% chance if she's being talked to
         botResponding = true;
-    } else if (message.toLowerCase().indexOf('angel') > -1 && user.toLowerCase() === 'zamiell') {
+    } else if (message.toLowerCase().indexOf('angelica') > -1 && user.toLowerCase() === 'zamiell') {
         botResponding = true;
     }
     if (botResponding === false) {
@@ -94,19 +119,19 @@ TwitchBot.on('chat', function(channel, user, message, self) {
     }
 
     // Generate a response from the brain
-    var cmd = botDirectory + '/scripts/get-response.py';
+    cmd = botDirectory + '/scripts/get-response.py';
     exec(cmd, [brain, message]);
 
     // Read what the response was
-    var response = fs.readFileSync(botDirectory + '/brains/' + brain + '-response.txt', 'utf8');
+    let response = fs.readFileSync(botDirectory + '/brains/' + brain + '-response.txt', 'utf8');
 
     // Replace any instances of Angelica with the name of the user that talked
     response = response.replace(/(angelica_e|angelica)/ig, user);
 
     // Replace any instance of "@username" with the name of the user that talked
-    var m = response.match(/@([a-zA-Z0-9_]{4,25})/); // From: https://www.reddit.com/r/Twitch/comments/32w5b2/username_requirements/
+    let m = response.match(/@([a-zA-Z0-9_]{4,25})/); // From: https://www.reddit.com/r/Twitch/comments/32w5b2/username_requirements/
     if (m) {
-        var person = m[1];
+        let person = m[1];
 
         // Check to see if they are in the channel
         if (channelUserList[channel].indexOf(person) === -1) {
@@ -141,7 +166,7 @@ TwitchBot.on('part', function(channel, username, self) {
     }
 
     // Keep track of the people in this channel
-    var index = channelUserList[channel].indexOf(username);
+    let index = channelUserList[channel].indexOf(username);
     if (index > -1) {
         channelUserList[channel].splice(index, 1);
     } else {
